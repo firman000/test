8ebbe72290541199fcdbe53399dcc66969d2ab04
firman000 <achmadfirman32@gmail.com>
2023-06-04 19:57:49 +0700

    upload test

---
+++ banner.js
@@ -0,0 +1,9 @@
+
+	atOptions = {
+		'key' : '8b6f4e11989b251731f1c838446890db',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://kindnessmarshalping.com/8b6f4e11989b251731f1c838446890db/invoke.js"></scr' + 'ipt>');
